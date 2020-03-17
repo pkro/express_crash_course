cc02575b59768406ba8d4c9403fe390797e09be0
pkro <pkrombach@web.de>
2020-03-17 13:27:06 +0100

    CRUD done

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const members = require("../../Members");
-const uuid = require('uuid');
+let members = require("../../Members");
+const uuid = require("uuid");
 
 const router = express.Router();
 
@@ -12,27 +12,47 @@ router.get("/", (req, res) => {
 // get single member
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  const member = members.find(entry => entry.id === parseInt(id))
-  if(member) {
+  const member = members.find(entry => entry.id === parseInt(id));
+  if (member) {
     res.json(member);
   } else {
-    res.status(400).json({msg: `member ${id} not found`});
+    res.status(400).json({ msg: `member ${id} not found` });
   }
 });
 
-
 // Create member
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const newMember = {
     id: uuid.v4(),
     name: req.body.name,
     email: req.body.email,
-    status: 'active' ,
-  }
-  if(!newMember.name || !newMember.email) {
-    return res.status(400).json({msg: 'Please include name and email'});
+    status: "active"
+  };
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: "Please include name and email" });
   }
   members.push(newMember);
+  res.json(members);
+});
+
+// Update member
+router.put("/", (req, res) => {
+  const id = req.body.id;
+  const member = members.find(entry => entry.id === parseInt(id));
+  if (!member) {
+    return res.status(400).json({ msg: "Member not found" });
+  }
+  member.name = req.body.name || member.name;
+  member.email = req.body.email || member.email;
+  member.status = req.body.status || member.status;
+
+  res.status(200).json({ msg: "success", member: member });
+});
+
+// Delete member
+router.delete("/", (req, res) => {
+  const id = req.body.id;
+  members = members.filter(member => member.id !== id);
   res.json(members);
 });
 
