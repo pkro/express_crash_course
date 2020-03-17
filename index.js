a56039d5459bf280c544494af816eae50fb77f97
pkro <pkrombach@web.de>
2020-03-17 14:10:04 +0100

    basic member table view

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
+const members = require('./Members');
 const logger = require("./middleware/logger");
 
 const app = express();
@@ -7,12 +9,23 @@ const app = express();
 //init middleware
 app.use(logger);
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  const title = 'Member app';
+  res.render('index', {
+    title,
+    members
+  });
+});
 
 // members API
-app.use('/api/members/', require('./routes/api/members'));
+app.use("/api/members/", require("./routes/api/members"));
 
 // set static folder - already enough to serve all files from this folder
 app.use(express.static(path.join(__dirname, "public")));
