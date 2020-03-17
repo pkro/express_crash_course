e025fd060ad244e23bcaf5764d4e553136e43bc3
pkro <pkrombach@web.de>
2020-03-17 10:01:35 +0100

    set up serving from static folder

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  const file = path.join(__dirname, "public", "index.html");
-  console.log(file);
-  res.sendFile(file);
-});
+// set static folder - already enough to serve all files from this folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
