cb2c6ca813125d7abeec6948eeec011f394cf170
pkro <pkrombach@web.de>
2020-03-17 10:43:50 +0100

    moved response test content

---
+++ Members.js
@@ -0,0 +1,7 @@
+const members = [
+  { id: 1, name: "John Doe", email: "a@bc.de", status: "active" },
+  { id: 2, name: "Jane Doe", email: "b@bc.de", status: "active" },
+  { id: 3, name: "Frank Doe", email: "c@bc.de", status: "inactive" }
+];
+
+module.exports = members;
