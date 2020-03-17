3c10134cfe5071e7a6c49fa9b1aa105435acc1c8
pkro <pkrombach@web.de>
2020-03-17 09:59:07 +0100

    serve static file basic

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<b>Yay</b>');
-  res.end();
-})
+app.get("/", (req, res) => {
+  const file = path.join(__dirname, "public", "index.html");
+  console.log(file);
+  res.sendFile(file);
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
